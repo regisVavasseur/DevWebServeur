f4b19a7a5f2dd64ce25ff02880f430c3e0e20f26
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-09-12 16:34:01 +0200

    adjustment exercise 1 to make it work

--- shop.pizza-shop/src/domain/service/commande/CommandeService.php
+++ shop.pizza-shop/src/domain/service/commande/CommandeService.php
@@ -1,29 +1,34 @@
 <?php
 
+use pizzashop\shop\domain\dto\order\OrderDTO;
+
 class CommandeService
 {
 
     public function readOrder($id_commande)
     {
-        /*
         $orderDTO = OrderDTO::find($id_commande);
         if ($orderDTO == null) {
             throw new ServiceCommandeNotFoundException();
         }
         return $orderDTO;
-        */
+
     }
 
     public function validateOrder($id_commande)
     {
-        /*
+
         $orderDTO = OrderDTO::find($id_commande);
         if ($orderDTO == null) {
             throw new ServiceCommandeNotFoundException();
         }
+        if ($orderDTO->etat != 1) {
+            throw new ServiceCommandeInvalidException();
+        }
         $orderDTO->etat = 2;
         $orderDTO->save();
-        */
+        return $orderDTO;
+
     }
 
 
