3e7435b8a584aef832002f7cfa0d9f45140eb8e1
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-09-12 16:24:01 +0200

    init Ex1

---
+++ shop.pizza-shop/src/domain/service/CommandeService.php
@@ -0,0 +1,32 @@
+<?php
+
+class CommandeService
+{
+
+    public function readOrder($id_commande)
+    {
+        /*
+        $orderDTO = OrderDTO::find($id_commande);
+        if ($orderDTO == null) {
+            throw new ServiceCommandeNotFoundException();
+        }
+        return $orderDTO;
+        */
+    }
+
+    public function validateOrder($id_commande)
+    {
+        /*
+        $orderDTO = OrderDTO::find($id_commande);
+        if ($orderDTO == null) {
+            throw new ServiceCommandeNotFoundException();
+        }
+        $orderDTO->etat = 2;
+        $orderDTO->save();
+        */
+    }
+
+
+
+
+}
---
+++ shop.pizza-shop/src/domain/service/ServiceCommandeNotFoundException.php
@@ -0,0 +1,6 @@
+<?php
+
+class ServiceCommandeNotFoundException extends Exception
+{
+
+}
