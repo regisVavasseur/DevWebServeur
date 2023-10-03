7863a03a2ed4c8703e5150807938f37bbe88cb4b
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-10-03 17:21:48 +0200

    Réalisation de l'exercice 5 du TD3

--- shop.pizza-shop/config/routes.php
+++ shop.pizza-shop/config/routes.php
@@ -7,7 +7,7 @@ use pizzashop\shop\app\controllers\PatchValiderCommandeAction;
 
 return function( \Slim\App $app):void {
 
-    //$app->post('/commandes[/]', \pizzashop\shop\app\actions\CreerCommandeAction::class)->setName('creer_commande');
+    $app->post('/commandes[/]', \pizzashop\shop\app\actions\CreerCommandeAction::class)->setName('creer_commande');
 
     $app->patch('/commandes/{id_commande}[/]', PatchValiderCommandeAction::class)->setName('commande');
 
