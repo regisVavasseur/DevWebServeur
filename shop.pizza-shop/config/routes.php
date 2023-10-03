af5291dac2a05f099c3bbfe7974ea1c73c0e0e36
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-10-03 17:13:16 +0200

    Début correction TD3 #2

--- shop.pizza-shop/config/routes.php
+++ shop.pizza-shop/config/routes.php
@@ -2,14 +2,14 @@
 declare(strict_types=1);
 
 use pizzashop\shop\app\contollers\GetCommandeAction;
-use pizzashop\shop\app\controllers\getValiderCommandeAction;
+use pizzashop\shop\app\controllers\PatchValiderCommandeAction;
 
 
 return function( \Slim\App $app):void {
 
     //$app->post('/commandes[/]', \pizzashop\shop\app\actions\CreerCommandeAction::class)->setName('creer_commande');
 
-    $app->patch('/commandes/{id_commande}[/]', getValiderCommandeAction::class)->setName('commande');
+    $app->patch('/commandes/{id_commande}[/]', PatchValiderCommandeAction::class)->setName('commande');
 
     $app->get('/commandes/{id}[/]', GetCommandeAction::class)->setName('commandes');
 };
