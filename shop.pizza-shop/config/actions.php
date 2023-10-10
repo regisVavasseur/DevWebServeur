53cd10c3637b241646221ed18246c5220b3c0332
Yvan <yvanquilliard@hotmail.fr>
2023-10-10 14:54:16 +0200

    add action to action.php

--- shop.pizza-shop/config/actions.php
+++ shop.pizza-shop/config/actions.php
@@ -1,3 +1,10 @@
 <?php
 
-// TODO: write code here
+use pizzashop\shop\app\action\GetCommandeAction;
+use Psr\Container\ContainerInterface;
+
+return [
+    GetCommandeAction::class => function(ContainerInterface $container) {
+    return new GetCommandeAction($container->get('commande.service'));
+    }
+];
