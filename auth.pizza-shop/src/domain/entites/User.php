32465e17b4af0be20489dde0ae6bd8086cdfa767
Yvan <yvanquilliard@hotmail.fr>
2023-10-17 17:52:20 +0200

    Add user model

--- auth.pizza-shop/src/domain/entites/User.php
+++ auth.pizza-shop/src/domain/entites/User.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
 
+    protected $fillable = [
+        'email',
+        'password',
+        'active',
+        'activation_token',
+        'activation_token_date',
+        'refresh_token',
+        'refresh_token_expiration_date',
+        'reset_passwd_token',
+        'reset_passwd_token_expiration_date',
+        'username'
+    ];
+    protected $table = 'users';
+    protected $primaryKey = 'email';
+    protected $keyType = 'string';
+    public $timestamps = false;
+
 }
