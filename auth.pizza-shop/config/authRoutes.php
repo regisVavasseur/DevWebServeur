463a47955778fae20585eb4577e0bde66315b821
Maxime48 <maxime4570@gmail.com>
2024-01-23 15:37:55 +0100

    Small route fix

--- auth.pizza-shop/config/authRoutes.php
+++ auth.pizza-shop/config/authRoutes.php
@@ -8,9 +8,9 @@ use pizzashop\auth\api\app\actions\RefreshTokenAction;
 
 return function(App $app) {
     $app->group('/api/users', function($group) {
-        $group->post('/signin', SignInAction::class);
-        $group->get('/validate', ValidateTokenAction::class);
-        $group->post('/refresh', RefreshTokenAction::class);
-        $group->post('/signup', SignUpAction::class);
+        $group->post('/signin[/]', SignInAction::class);
+        $group->get('/validate[/]', ValidateTokenAction::class);
+        $group->post('/refresh[/]', RefreshTokenAction::class);
+        $group->post('/signup[/]', SignUpAction::class);
     });
 };
