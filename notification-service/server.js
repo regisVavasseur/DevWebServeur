93829cf78f81ce956f75ed45f605f6d5e3b709fe
Maxime48 <maxime4570@gmail.com>
2024-01-26 17:13:59 +0100

    Fin tp10

--- notification-service/server.js
+++ notification-service/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const { WebSocketServer } = require('ws');
+const amqp = require('amqplib');
 
 const PORT = 3000;
+const AMQP_URL = 'amqp://admin:admin@rabbitmq';
+const QUEUE_NAME = 'suivi_commandes';
+
 const server = express()
-    .use((req, res) => res.send('Express server with WebSocket'))
+    .use(
+        (req, res) =>
+        res.send('Express server with WebSocket and RabbitMQ')
+    )
     .listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
 
 const wss = new WebSocketServer({ server });
-
-// Stocker les clients WebSocket en fonction de l'ID de commande
 const clients = {};
 
 wss.on('connection', (ws) => {
@@ -16,39 +21,43 @@ wss.on('connection', (ws) => {
 
     ws.on('message', (data) => {
         const message = data.toString();
-        console.log(`Message received: ${message}`);
-
-        // Supposons que le client envoie l'ID de commande pour s'abonner aux mises à jour
-        // avec un message comme "subscribe:<orderID>"
         const [action, orderId] = message.split(':');
-
         if (action === 'subscribe' && orderId) {
-            // Associer ce socket au orderId
             if (!clients[orderId]) {
                 clients[orderId] = [];
             }
             clients[orderId].push(ws);
-
             ws.send(`Subscribed to updates for order ${orderId}`);
-        } else {
-            console.error('Unknown action');
         }
-    });
-
-    ws.on('close', () => {
-        console.log('Client disconnected');
     });
 });
 
-// Fonction pour simuler la mise à jour d'une commande et notifier le client
-function simulateOrderUpdate(orderId, status) {
-    if (clients[orderId]) {
-        clients[orderId].forEach(client => {
-            client.send(`Order ${orderId} status: ${status}`);
+async function connectToRabbitMQ() {
+    try {
+        const conn = await amqp.connect(AMQP_URL);
+        console.log('Connected to RabbitMQ');
+
+        const channel = await conn.createChannel();
+        await channel.assertQueue(QUEUE_NAME, { durable: false });
+        console.log(`Waiting for messages in ${QUEUE_NAME}. To exit press CTRL+C`);
+
+        channel.consume(QUEUE_NAME, (msg) => {
+            if (msg !== null) {
+                console.log(`Received message: ${msg.content.toString()}`);
+                const { orderId, status } = JSON.parse(msg.content.toString());
+
+                if (clients[orderId]) {
+                    clients[orderId].forEach(client => {
+                        client.send(`Order ${orderId} status: ${status}`);
+                    });
+                }
+
+                channel.ack(msg);
+            }
         });
+    } catch(error) {
+        console.error('Error connecting to RabbitMQ:', error);
     }
 }
 
-// Simulation d'une mise à jour de commande
-setTimeout(() => { simulateOrderUpdate('123', 'Preparing') }, 60*1000);
-setTimeout(() => { simulateOrderUpdate('123', 'Ready for pickup') }, 120*1000);
+connectToRabbitMQ();
