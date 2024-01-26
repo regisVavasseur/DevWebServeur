5a4528fa1c2c3a0474fd24b17a87b5778527e17b
ElianThe <guiffaul2u@etu.univ-lorraine.fr>
2024-01-26 16:52:55 +0100

    changement du fichier server.js

--- production.pizza-shop/src/server.js
+++ production.pizza-shop/src/server.js
@@ -1,29 +1,17 @@
 //importation de l'application depuis le fichier de configuration des routes
-import app from "./config/routes.js";
-import mysql from 'mysql';
+import express from "express"
+import routeConfig from "../src/config/routes.js";
 
-const connection = mysql.createConnection({
-    host: 'pizza-shop.production.db',
-    user: 'production',
-    password: 'production',
-    database: 'production',
-    port : 3306
-});
+//creation d'une instance express
+const app = express();
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Erreur de connexion à la base de données:', err);
-    } else {
-        console.log('Connecté à la base de données MySQL!');
-        // Tu peux effectuer des opérations sur la base de données ici
-    }
-});
+routeConfig(app);
 
 // Définition du port sur lequel le serveur écoutera
 const port = process.env.port || 3000;
 
 //lancement du serveur et écoute sur le port défini
+
 app.listen(port, () => {
     console.log('Le serveur est en écoute sur le port ' + port);
 })
-connection.end();
